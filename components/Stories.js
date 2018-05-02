96021c8e6404225b6044d2f6663b69dc1b1f0840
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-05-02 22:35:46 +0900

    proper styling

--- components/Stories.js
+++ components/Stories.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import {
   Text,
-  View
+  View,
+  FlatList,
+  Image
 } from 'react-native';
 import axios from 'axios';
 
@@ -30,13 +32,27 @@ export default class Stories extends Component {
 
   render() {
     const { state } = this;
+    console.log(state.stories)
     return (
       <View>
-          {
-            state.stories.map((l, i) => (
-              <Text>{l.section}</Text>
-            ))
-          }
+        <FlatList
+          data={state.stories}
+          renderItem={({item, index}) => (
+            <View style={{flex: 1, flexDirection: 'row', paddingTop: 5, overflow: 'hidden'}}>
+              {item.multimedia[0] ? (
+                <Image
+                  style={{width: 50, height: 50, marginRight: 5, marginLeft: 5}}
+                  source={{uri: item.multimedia[0] && item.multimedia[0].url}}
+                />
+              ) : (
+                <View style={{width: 50, height: 50, backgroundColor: 'black', marginRight: 5, marginLeft: 5}} />
+              )}
+              <View style={{flex: 1, justifyContent: 'center'}}>
+                <Text>{item.title}</Text>
+              </View>
+            </View>
+          )}
+        />
       </View>
     );
   }
