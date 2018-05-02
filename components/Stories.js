1a88b112be918f7a09a751c540b07719cf0d48a8
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-05-03 00:34:21 +0900

    story webview

--- components/Stories.js
+++ components/Stories.js
@@ -3,7 +3,10 @@ import {
   Text,
   View,
   FlatList,
-  Image
+  Image,
+  WebView,
+  Linking,
+  TouchableHighlight
 } from 'react-native';
 import axios from 'axios';
 
@@ -12,8 +15,11 @@ export default class Stories extends Component {
   state = {
     stories: [],
     error: '',
-    isLoading: false
+    isLoading: false,
+    openWebView: false,
+    webViewUrl: ''
   };
+
   componentDidMount () {
     const _self = this;
     this.setState({ isLoading: true });
@@ -30,27 +36,51 @@ export default class Stories extends Component {
       });
   };
 
+  handleOpenWebView (url) {
+    console.log(url)
+    if (url) {
+      this.setState({ openWebView: true, webViewUrl: url });
+    }
+  };
+
   render() {
     const { state } = this;
-    console.log(state.stories)
+    if (state.openWebView) {
+      return (
+        <WebView
+          ref={(ref) => { this.webview = ref; }}
+          source={{ uri: state.webViewUrl }}
+          onNavigationStateChange={(event) => {
+            if (event.url !== state.webViewUrl) {
+              this.webview.stopLoading();
+              Linking.openURL(event.url);
+            }
+          }}
+        />
+      );
+    }
     return (
       <View>
         <FlatList
           data={state.stories}
           renderItem={({item, index}) => (
-            <View style={{flex: 1, flexDirection: 'row', paddingTop: 5, overflow: 'hidden'}}>
-              {item.multimedia[0] ? (
-                <Image
-                  style={{width: 50, height: 50, marginRight: 5, marginLeft: 5}}
-                  source={{uri: item.multimedia[0] && item.multimedia[0].url}}
-                />
-              ) : (
-                <View style={{width: 50, height: 50, backgroundColor: 'black', marginRight: 5, marginLeft: 5}} />
-              )}
-              <View style={{flex: 1, justifyContent: 'center'}}>
-                <Text>{item.title}</Text>
+            <TouchableHighlight
+              onPress={this.handleOpenWebView.bind(this, item.url)}
+              key={index}>
+              <View style={{flex: 1, flexDirection: 'row', paddingTop: 5, overflow: 'hidden'}}>
+                {item.multimedia[0] ? (
+                  <Image
+                    style={{width: 50, height: 50, marginRight: 5, marginLeft: 5}}
+                    source={{uri: item.multimedia[0] && item.multimedia[0].url}}
+                  />
+                ) : (
+                  <View style={{width: 50, height: 50, backgroundColor: 'black', marginRight: 5, marginLeft: 5}} />
+                )}
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                  <Text>{item.title}</Text>
+                </View>
               </View>
-            </View>
+            </TouchableHighlight>
           )}
         />
       </View>
